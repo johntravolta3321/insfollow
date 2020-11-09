13e53f0ef8f96b5aa284739e8ee899ab833bf2e9
johntravolta3321 <74209216+johntravolta3321@users.noreply.github.com>
2020-11-09 22:56:23 +0300

    Update insfollow.sh

--- insfollow.sh
+++ insfollow.sh
@@ -96,7 +96,6 @@ therock="232192182"
 kylie="12281817"
 jelopez="305701719"
 messi="427553890"
-
 dualipa="12331195"
 mileycyrus="325734299"
 shawnmendes="212742998"
@@ -112,7 +111,7 @@ instagram="25025320"
 
 
 if [[ ! -e celeb_id ]]; then
-printf "%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n" $dualipa $mileycyrus $shawnmendes $katyperry $charlieputh $lelepons $camila_cabello $madonna $leonardodicaprio $ladygaga $taylorswift $instagram $neymar $selena $ariana $beyonce $cristiano $kimkardashian $kendall $therock $kylie $jelopez $messi > celeb_id
+printf "%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n" $mileycyrus $shawnmendes $katyperry $charlieputh $lelepons $camila_cabello $madonna $leonardodicaprio $ladygaga $taylorswift $instagram $neymar $selena $ariana $beyonce $cristiano $kimkardashian $kendall $therock $kylie $jelopez $messi > celeb_id
 fi
 
 while [[ true ]]; do
